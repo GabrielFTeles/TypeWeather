75e2a37ae9fc533a29b486b7dbb688929426985a
Gabriel Teles <gabrielfariateles@gmail.com>
2023-09-19 22:39:30 -0300

    🚀 Project Finished.

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import { Spin } from "../Spin";
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+  isLoading?: boolean;
+}
+
+export function Input({ isLoading = false, ...rest }: Props) {
+  return (
+    <div className="input">
+      <input type="text" {...rest} />
+
+      {isLoading && <Spin />}
+    </div>
+  );
+}
